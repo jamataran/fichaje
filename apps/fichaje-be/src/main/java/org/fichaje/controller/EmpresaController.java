c31385fdd2903abc10fa38d9c8d6349b2fe04136
Daviid <dvd.dmu@gmail.com>
2026-03-16 12:48:32 +0100

    feat: Refactor EmpresaController and EmpresaService to streamline DTO handling and improve response structure

--- apps/fichaje-be/src/main/java/org/fichaje/controller/EmpresaController.java
+++ apps/fichaje-be/src/main/java/org/fichaje/controller/EmpresaController.java
@@ -1,12 +1,10 @@
 package org.fichaje.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import org.fichaje.converter.EmpresaDtoConverter;
 import org.fichaje.dto.entity.EmpresaDTO;
 import org.fichaje.provider.db.entity.Empresa;
 import org.fichaje.service.EmpresaService;
 import org.springframework.data.domain.Page;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,60 +15,41 @@ import java.net.URI;
 @RestController
 @RequestMapping("/empresas")
 public class EmpresaController
-		extends CommonController<Empresa, EmpresaService> {
+        extends CommonController<Empresa, EmpresaService> {
 
-	@Autowired
-	EmpresaDtoConverter dtoConverter;
+    @Operation(summary = "Crea una nueva empresa")
+    @PostMapping
+    public ResponseEntity<EmpresaDTO> newEmpresa(
+            @RequestBody EmpresaDTO empresaDto) {
+        EmpresaDTO empresaGuardada = service.save(empresaDto);
 
-	@Operation(summary = "Crea una nueva empresa")
-	@PostMapping
-	public ResponseEntity<EmpresaDTO> newEmpresa(
-			@RequestBody EmpresaDTO empresaDto) {
-		Empresa empresaGuardada = service.save(dtoConverter.transform(empresaDto));
-		EmpresaDTO responseDTO = dtoConverter.inverseTransform(empresaGuardada);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(empresaGuardada.getId())
+                .toUri();
 
-		URI    location = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(empresaGuardada.getId())
-				.toUri();
+        return ResponseEntity.created(location).body(empresaGuardada);
+    }
 
-		return ResponseEntity.created(location).body(responseDTO);
-	}
+    @Operation(summary = "Devuelve una lista de DTO de empresas")
+    @GetMapping
+    public ResponseEntity<Page<EmpresaDTO>> list(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
 
-	@Operation(summary = "Devuelve una lista de DTO de empresas")
-	@GetMapping
-	public ResponseEntity<Page<EmpresaDTO>> list(Pageable pageable) {
-		return ResponseEntity.ok(service.findAll(pageable).map(dtoConverter::inverseTransform));
-	}
+    @Operation(summary = "Edita una empresa")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editEmpresa(@RequestBody EmpresaDTO empresa, @PathVariable Long id) {
 
-	@Operation(summary = "Edita una empresa")
-	@PutMapping("/{id}")
-	public ResponseEntity<?> editEmpresa(@RequestBody Empresa editar,
-										 @PathVariable Long id) {
+        return ResponseEntity.ok(service.update(empresa, id));
+    }
 
-		return service.findById(id).map(c -> {
-			c.setId(editar.getId());
-			c.setNombre(editar.getNombre());
-			c.setCif(editar.getCif());
-			c.setActiva(editar.isActiva());
-
-			return ResponseEntity.ok(service.save(c));
-		}).orElseGet(() -> {
-			return ResponseEntity.notFound().build();
-		});
-	}
-
-	@Override
-	@Operation(summary = "Elimina una empresa")
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) {
-		return service.findById(id).map(c -> {
-			service.delete(id);
-			return ResponseEntity.noContent().build();
-		}).orElseGet(() -> {
-			return ResponseEntity.notFound().build();
-		});
-	}
+    @Operation(summary = "Elimina una empresa")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- apps/fichaje-be/src/main/java/org/fichaje/service/EmpresaService.java
+++ apps/fichaje-be/src/main/java/org/fichaje/service/EmpresaService.java
@@ -1,25 +1,64 @@
 package org.fichaje.service;
 
+import org.fichaje.converter.EmpresaDtoConverter;
+import org.fichaje.dto.entity.EmpresaDTO;
 import org.fichaje.provider.db.entity.Empresa;
 import org.fichaje.provider.db.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
+
 import java.util.Optional;
 
 @Service
 @Transactional
 public class EmpresaService
-		extends CommonServiceImpl<Empresa, EmpresaRepository> {
+        extends CommonServiceImpl<Empresa, EmpresaRepository> {
 
-	public Optional<Empresa> findByCif(String cif) {
-		return repository.findByCif(cif);
-	}
+    @Autowired
+    EmpresaDtoConverter dtoConverter;
 
-	public Optional<Empresa> findByActiva(Boolean activa) { return repository.findByActiva(activa); }
+    @Autowired
+    EmpresaRepository repository;
 
-	public Page<Empresa> findAll(Pageable pageable) { return repository.findAll(pageable); }
+    public Optional<Empresa> findByCif(String cif) {
+        return repository.findByCif(cif);
+    }
+
+    public Optional<Empresa> findByActiva(Boolean activa) {
+        return repository.findByActiva(activa);
+    }
+
+    public Page<EmpresaDTO> findAll(Pageable pageable) {
+        return repository.findAll(pageable).map(dtoConverter::inverseTransform);
+    }
+
+    public EmpresaDTO save(EmpresaDTO empresaDTO) {
+        Empresa empresa = dtoConverter.transform(empresaDTO);
+        Empresa savedEmpresa = repository.save(empresa);
+        return dtoConverter.inverseTransform(savedEmpresa);
+    }
+
+    public EmpresaDTO update(EmpresaDTO empresaDTO, Long id) {
+        return repository.findById(id).map(
+                c -> {
+                    c.setId(empresaDTO.getId());
+                    c.setNombre(empresaDTO.getNombre());
+                    c.setCif(empresaDTO.getCif());
+                    c.setActiva(empresaDTO.isActiva());
+
+                    Empresa updatedEmpresa = repository.save(c);
+                    return dtoConverter.inverseTransform(updatedEmpresa);
+                }).orElseThrow(() -> new RuntimeException("Empresa no encontrada con id: " + id));
+    }
+
+    public void delete(Long id) {
+        repository.findById(id).orElseThrow(() -> new RuntimeException("Empresa no encontrada con id: " + id));
+        repository.deleteById(id);
+    }
 }
